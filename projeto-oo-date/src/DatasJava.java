c17d93b63963ee81efa7c69fb6f15dcd821be920
rodrigo <prof-rodrigo-silva@users.noreply.github.com>
2021-11-19 22:16:06 -0200

    Atualização

--- projeto-oo-date/src/DatasJava.java
+++ projeto-oo-date/src/DatasJava.java
@@ -1,11 +1,19 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DatasJava {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, InterruptedException {
 		
 Date date = new Date();
 		
@@ -89,6 +97,103 @@ Date date = new Date();
 		}else {
 			System.out.println("Boleto não vencido");
 		}
+		
+		System.out.println("--------------------------------------------------");
+		
+		long dias = ChronoUnit.DAYS.between(LocalDate.parse("2021-01-01"), LocalDate.now());
+		
+		System.out.println("Passaram "+dias+" dias...");
+		 
+		long meses = ChronoUnit.MONTHS.between(LocalDate.parse("2021-01-01"), LocalDate.now());
+		 
+		System.out.println("Passaram "+meses+" meses...");
+		
+		System.out.println("--------------------------------------------------");
+		
+		Calendar calendar1 = Calendar.getInstance();
+		
+		calendar1.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("19-11-2021"));
+		
+		calendar1.add(Calendar.DAY_OF_MONTH, -11);
+		
+		System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar1.getTime()));
+		
+		calendar1.add(Calendar.MONTH, 2);
+		
+		System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar1.getTime()));
+		
+		Calendar calendar2 = Calendar.getInstance();
+		Date dataInicial = new SimpleDateFormat("yyyy-MM-dd").parse("2021-11-19");
+		
+		calendar2.setTime(dataInicial);
+		
+		for(int i=1;i<=12;i++) {
+			calendar2.add(Calendar.MONTH, 1);
+			System.out.println("Parcela número: "+i+" vencimento é em: "
+					+new SimpleDateFormat("dd/MM/yyyy").format(calendar2.getTime()));
+			
+		}
+		System.out.println("--------------------------------------------------");
+		//API de data JAVA 8
+		
+		LocalDate dataHoje = LocalDate.now();
+		System.out.println("Data: "+dataHoje);
+		
+		LocalTime horaAtual = LocalTime.now();
+		System.out.println("Hora: "+horaAtual);
+		
+		LocalDateTime dataHora = LocalDateTime.now();
+		System.out.println("Data e Hora: "+dataHora);
+		
+		//Formatando
+		System.out.println("Data e Hora formatados: "+dataHora.format(DateTimeFormatter.BASIC_ISO_DATE));
+		System.out.println("Data e Hora formatados: "+dataHora.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		LocalDate localDate = LocalDate.now();
+		System.out.println("Data atual formatada: "+localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("Dia da semana: "+localDate.getDayOfWeek());
+		System.out.println("Dia do mês: "+localDate.getDayOfMonth());
+		System.out.println("Dia do ano: "+localDate.getDayOfYear());
+		System.out.println("Mês: "+localDate.getMonth());
+		System.out.println("Ano: "+localDate.getYear());
+		
+		//tempos
+		
+		Instant inicio = Instant.now();
+		
+		Thread.sleep(1000);
+		
+		Instant ifinal = Instant.now();
+		
+		Duration duration = Duration.between(inicio, ifinal);
+		
+		System.out.println("Nano: "+duration.toNanos());
+		System.out.println("Milesegundo: "+duration.toMillis());
+		System.out.println("Minutos: "+duration.toMinutes());
+		System.out.println("Hora: "+duration.toHours());
+		
+		//Períodos entre datas
+		LocalDate localDate2 = LocalDate.of(2019, 7, 1);
+		LocalDate localDate3 = LocalDate.of(2021, 11, 19);
+		
+		System.out.println("Data antiga é maior que data nova: "+localDate2.isAfter(localDate3));
+		System.out.println("Data antiga é anterior a data nova: "+localDate2.isBefore(localDate3));
+		System.out.println("Datas iguais: "+localDate2.isEqual(localDate3));
+		
+		Period period = Period.between(localDate2, localDate3);
+		System.out.println("Período é: "+period.getYears()+" anos "+period.getMonths()+" meses e "
+				+period.getDays()+" dias");
+		System.out.println(period.toTotalMonths());
+		
+		//Add tempos
+		LocalDate dataBase = LocalDate.parse("2021-11-19");
+		System.out.println("Data base: "+dataBase);
+		System.out.println("Mais 5 dias: "+(dataBase = dataBase.plusDays(5)));
+		System.out.println("Mais 4 semana: "+(dataBase = dataBase.plusWeeks(4)));
+		System.out.println("Mais 7 meses: "+(dataBase = dataBase.plusMonths(7)));
+		System.out.println("Mais 2 anos : "+(dataBase = dataBase.plusYears(2)));
+		System.out.println("Menos 1 ano: "+(dataBase = dataBase.minusYears(1)));
 		
 	}
 
